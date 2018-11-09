0baab655fc9beeac2ac891d85619718b658fee5a
HaiNguyen <hainhbkhn@gmail.com>
2018-11-08 23:33:32 -0800

    TLE inversion counter (expected)

---
+++ INVCNT.py
@@ -0,0 +1,40 @@
+def countInversion(arrIn):
+	inversionCnt = 0
+
+	while(True):
+		n = len(arrIn)
+
+		if (n == 1):
+			break
+
+		indexOfMax = arrIn.index(max(arrIn))
+		inversionCnt += len(arrIn) - indexOfMax - 1
+
+		del arrIn[indexOfMax]
+
+	print(inversionCnt)
+
+def main():
+
+	numOfTests = int(input())
+	input()
+
+	for i in range(numOfTests + 1):
+
+		if (i >= 1):
+			input()
+
+		numOfElement = int(input())
+
+		arrIn = []
+
+		for cnt in range(numOfElement):
+			temp = input()
+
+			if (temp != ' '):
+				arrIn.append(int(temp))
+
+		countInversion(arrIn)
+
+if __name__ == "__main__":
+    main()
